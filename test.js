3b00ab96075269da871f6e320e4bc6dae63b0f55
Sergey Kulinich <kulinicsergej870@gmail.com>
2022-04-16 10:24:28 +0300

    init

---
+++ test.js
@@ -0,0 +1,25 @@
+// for (let a = 0; a < 10; a++) {
+// 	a % 2 && console.log(a);
+// }
+
+// let letters="Fear the Donians who bring gifts"; 
+// let Word=(letters.split(' '));
+// let NewWord=Word.filter(function(Word){
+//      return Word.length>6
+// })
+// console.log(NewWord)
+
+
+// 3) Написать прогу, которая высчитает факториал любого числа, которое я передам.
+// const factotial = n =>
+// n<0 ?
+// (()=>{
+//      throw new TypeError('не то пальто')
+// })()
+// :
+// n<=1
+// 1 : n* factotial(n-1)
+// console.log(factotial(1));
+// console.log(factotial(-4));
+// confirm.log(56));
+
